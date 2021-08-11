81dd4cd5ed0feec7575324579ca4d02636278049
sagar <bhattsagar112@gmail.com>
2021-08-11 18:34:13 +0530

    REFS: #434 and fixes #434 Completed Day1 Assignment and Practice Exercise

--- SQL/Sagar Bhatt/Day1/DemoDatabaseSagar.sql
+++
@@ -1,9 +0,0 @@
---Create Database
-CREATE DATABASE Sagar;
-
---Create Table
-CREATE TABLE BioData(
-	P_Id int constraint PK_BioData PRIMARY KEY IDENTITY(1,1),
-	P_Name varchar(50) NOT NULL,
-	P_Mobile 
-);
