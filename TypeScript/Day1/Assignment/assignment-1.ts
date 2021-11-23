01d9cfbb691c9108a19afd40efd89f50254da785
sagar <bhattsagar112@gmail.com>
2021-11-23 23:35:31 +0530

    REFS: #2194 and fixes #2194 Tasks: Completed What is Typescript, Enivronment setup, Datatypes, Type Annotation, Number, Number Methods, String/Template String, String Methods, Any, Array /Array Methods with assignment.

---
+++ TypeScript/Day1/Assignment/assignment-1.ts
@@ -0,0 +1,41 @@
+// *  Store 5 employees’ data in one array (ID,FirstName,LastName,Address,Salary). Do the operation searching by indexnumber, EmployeeID, Insert the employee, delete the employee from the Array. Create one more array emp and join the value with above array. When display list combine firstname and lastname as fullname, From the address field flatnumber,city,state and should be splited.PF should be computed and total salary should be display.
+
+const empData: object[] = [
+  {
+    id: 1,
+    firstName: "sagar",
+    lastName: "bhatt",
+    address: "shahpur",
+    salary: 100000,
+  },
+  {
+    id: 2,
+    firstName: "harsh",
+    lastName: "bhatt",
+    address: "shahpur",
+    salary: 100000,
+  },
+  {
+    id: 3,
+    firstName: "khushubu",
+    lastName: "bhatt",
+    address: "shahpur",
+    salary: 100000,
+  },
+  {
+    id: 4,
+    firstName: "niraj",
+    lastName: "surati",
+    address: "shahpur",
+    salary: 100000,
+  },
+  {
+    id: 5,
+    firstName: "meet",
+    lastName: "bhatt",
+    address: "mainagar",
+    salary: 100000,
+  },
+];
+
+console.log(empData);
---
+++ TypeScript/Day1/Practical/type-annotation.ts
@@ -0,0 +1,100 @@
+// * TypeAnnotation
+
+// * number
+let first: number = 5;
+let bin: number = 0b111001;
+let oct: number = 0o377;
+let hex: number = 0x37cf;
+console.log(`number: ${first}, ${bin}, ${oct}, ${hex}`);
+
+// * string
+let str: string = `First: ${first}`;
+console.log(`string: ${str}`);
+
+// * boolean
+let isAdmin: boolean = true;
+console.log(`boolean: ${isAdmin}`);
+
+// * null
+let n: null = null;
+console.log(`null: ${n}`);
+
+// * void
+function funVoid(): void {
+  console.log(`void: Nothing to return!`);
+}
+funVoid();
+
+// * any: Store any type of data
+let data: any = 11;
+console.log(`any: ${data}`);
+
+// * Array: Collection of same data type
+let arrNum: number[] = [1, 2, 3];
+// Generic Array Type
+let arrStr: Array<string> = ["sagar", "harsh", "khushubu"];
+// Multi Type Array
+let arrMultiNum: (number | string)[] = ["sagar", "harsh", "khushubu", 1];
+let arrMultiStr: Array<string | number> = ["sagar", "harsh", "khushubu", 1];
+
+console.log(`array of number[]:`, arrNum);
+console.log(`array of string[] - Generic Array Type:`, arrStr);
+console.log(`array of number|string[]:`, arrMultiNum);
+console.log(`array of number|string[] - Generic Array Type:`, arrMultiStr);
+
+// * Methods
+// * number methods
+let no1 = 999.11111;
+let no2 = 999.99999;
+console.log(
+  no1.toFixed(2),
+  no2.toFixed(2),
+  no1.toLocaleString(),
+  no2.toLocaleString(),
+  no1.toString(),
+  no2.toString(),
+  no1.valueOf(),
+  no2.valueOf()
+);
+
+// * array methods
+let arr1: (number | string)[] = [2, 1, "sagar", "harsh"];
+let arr2: (number | string)[] = ["khushbu", "zeel", 3, 4];
+console.log(arr1, arr2);
+console.log(
+  arr1.concat(arr2),
+  arr1.copyWithin(0, 1),
+  arr1.pop(),
+  arr1.push("harsh"),
+  arr1,
+  // arr1.sort(),
+  // arr2.sort(),
+  arr1.indexOf("sagar"),
+  arr1.shift(),
+  arr1.fill(2, 1, 3)
+);
+
+let a = arr2.filter((elem) => elem > 2);
+console.log(a);
+
+// * string methods
+let str1: string = "Learning TypeScript";
+let str2: string = "string methods.";
+console.log(str1);
+console.log(
+  str1.charAt(0),
+  str1.concat(` ${str2}`),
+  str1.indexOf("typescript"),
+  str1.indexOf("TypeScript", 5),
+  str1.split(""),
+  str1.split(" "),
+  str1.toUpperCase(),
+  str1.toLowerCase(),
+  str1.includes("learning"),
+  str1.includes("Learning"),
+  str1.endsWith("TypeScript"),
+  str1.endsWith("t"),
+  str1.normalize(),
+  str1.repeat(2),
+  str1.slice(0, 8)
+);
